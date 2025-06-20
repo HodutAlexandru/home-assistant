280431f0f538d2f36c20c5ac227b67e72dba0016
hodut.alexandru <hodut.alexandru@gmail.com>
2025-06-20 14:28:17 +0300

    Add logging

--- src/main/java/com/home/ai/assistant/controller/HealthController.java
+++ src/main/java/com/home/ai/assistant/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.home.ai.assistant.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/health")
 public class HealthController {
 
+    private static final Logger log = LoggerFactory.getLogger(HealthController.class);
+
     @GetMapping
     public String checkHealthy() {
+        log.info("Service up and running!");
         return "Status UP!";
     }
 
